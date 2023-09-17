9f6f57cad24c2613ce6dab5a0d17a2d9a9e93603
DEV1lmig <maquiroz@urbe.edu.ve>
2023-09-17 15:54:33 -0400

    first commit

--- src/app/components/product-item.js
+++ src/app/components/product-item.js
@@ -2,34 +2,26 @@ import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
 
-const imageStyle = {
-  borederRadius: '20%',
-  width: '300px',
-  height: '300px',
-  objectFit: 'cover'
-}
-
 export default function ProductItem ({ product }) {
   return (
-    <div className='card'>
+    <div className='card-body card-normal card-bordered'>
       <Link href={`/product/${product.slug}`}>
         <Image
           src={product.image}
-          width={200}
-          height={200}
-          style={imageStyle}
+          width={500}
+          height={500}
           alt={product.name}
-          className='rounded shadow'
+          className='rounded shadow w-42 h-60 object-cover'
         />
       </Link>
 
       <div className='flex flex-col items-center justify-center p-5'>
-        <Link href={`/product/${product.slug}`}>
+        <Link href={`/product/${product.slug}`} className='card-title'>
           <h2 className='text.lg'>{product.name}</h2>
         </Link>
         <p className='mb-2'>{product.brand}</p>
         <p className='mb-2'>${product.price}</p>
-        <button className='primary-button' type='button'>
+        <button className='btn btn-primary items-center card-actions' type='button'>
           Add to cart
         </button>
       </div>
